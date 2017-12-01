9041f057c8ffaf0ed275160ebf2a8d4547cb9291
Jen Chow <jennego@gmail.com>
2017-11-30 19:40:05 -0800

    Removed open weather api, replaced with dark sky api Added week forecast.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,17 +2,11 @@ class WelcomeController < ApplicationController
 
   def index
     @posts = Post.all
-    require 'open_weather'
-    require 'date'
-    options = { units: "metric", APPID: ENV['OPEN_WEATHER'] }
-    @weather = OpenWeather::Current.city_id("6173331", options)
-    @forecast = OpenWeather::Forecast.city_id("6173331", options)
 
-require 'forecast_io'
+   require 'forecast_io'
 
     ForecastIO.api_key = ENV['DARKSKY_API']
-
-    @darksky = ForecastIO.forecast(49.2827, 123.1207, params: { units: 'si' })
+    @darksky = ForecastIO.forecast(49.2827, -123.1207, params: { units: 'ca' })
 
 
 
