a3e15ff0c94a400204a35411ffcb45e0e339e1ea
Jen Chow <jennego@gmail.com>
2017-12-11 15:37:24 -0800

    Put reminders as before action for users and cal controllers

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,5 +1,6 @@
 class CalendarController < ApplicationController
 before_action :authenticate_user!
+before_action :define_reminders
     def index
       @posts = Post.where(user: current_user)
       @reminders = Reminder.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+before_action :define_reminders
 
   def dashboard
 
