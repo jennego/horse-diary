05edafabd6831d8bd901993a4ee62e573c1269a8
Jen Chow <jennego@gmail.com>
2017-12-12 18:01:05 -0800

    Add amazon SES for mailers

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,18 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: '587',
+  address: 'mail-smtp.us-west-2.amazonaws.com',
+  port: '465',
   enable_starttls_auto: true,
   authentication: :plain,
-  user_name: ENV['EMAIL_USER_NAME'],
-  password: ENV['EMAIL_PASSWORD']
+  user_name: ENV['SES_USER_NAME'],
+  password: ENV['SES_PASSWORD']
 }
+
+#
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.gmail.com',
+#   port: '587',
+#   enable_starttls_auto: true,
+#   authentication: :plain,
+#   user_name: ENV['EMAIL_USER_NAME'],
+#   password: ENV['EMAIL_PASSWORD']
+# }
