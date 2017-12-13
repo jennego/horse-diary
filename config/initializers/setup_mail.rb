9cffb118f6c35cbf475a343fb7987153356ff277
Jen Chow <jennego@gmail.com>
2017-12-13 00:26:50 -0800

    Change to and intergrate with Sendgrid

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,13 +1,24 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'mail-smtp.us-west-2.amazonaws.com',
+  address: 'smtp.sendgrid.net',
   port: '465',
   enable_starttls_auto: true,
   authentication: :plain,
-  user_name: ENV['SES_USER_NAME'],
-  password: ENV['SES_PASSWORD']
+  user_name: ENV['SENDGRID_USER_NAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  ssl:                   true,
+  tls:                   true
 }
 
-#
+# ActionMailer::Base.smtp_settings = {
+#   address: 'mail-smtp.us-west-2.amazonaws.com',
+#   port: '465',
+#   enable_starttls_auto: true,
+#   authentication: :login,
+#   user_name: ENV['SES_USER_NAME'],
+#   password: ENV['SES_PASSWORD']
+# }
+
+
 # ActionMailer::Base.smtp_settings = {
 #   address: 'smtp.gmail.com',
 #   port: '587',
